d56f3e5b42968d4d55b49c3b412449de3a38a247
abhishektiwari283 <142007480+Code-writter@users.noreply.github.com>
2025-08-14 15:26:41 +0530

    basics arithemtic operations and the assignment operators

---
+++ 01_Basics/02_Numbers/index.go
@@ -0,0 +1,23 @@
+package main
+
+
+import "fmt"
+
+func main(){
+	fmt.Println(2 + 2)
+	fmt.Println(2 - 2)
+	fmt.Println(2 * 2)
+	fmt.Println(2 / 2)
+	fmt.Println(2 % 2)
+
+	num := 10
+	num++
+	fmt.Println(num)
+	num--
+	fmt.Println(num)
+
+
+	var number int32 = 2000 
+
+	fmt.Println(number)
+}
